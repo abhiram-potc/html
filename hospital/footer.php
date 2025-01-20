7dc28c6cff9c5f0bc0a73a98760ca006c809cc99
Abhiram <abhirammohanpotc00@gmail.com>
2025-01-20 14:08:23 +0530

    Delete hospital directory

--- hospital/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Your Company Name. All rights reserved.</p>
-    <p><a href="privacy.html">Privacy Policy</a> | <a href="terms.html">Terms of Service</a></p>
-</footer>
